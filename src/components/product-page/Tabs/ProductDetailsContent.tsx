5be132cf6b2092ac34ed8a6b5059e5d866e451f0
wyh157364808-creator <wyh157364808@gmail.com>
2026-09-16 19:03:55 +0800

    Update ProductDetailsContent.tsx

--- src/components/product-page/Tabs/ProductDetailsContent.tsx
+++ src/components/product-page/Tabs/ProductDetailsContent.tsx
@@ -18,7 +18,8 @@ const ProductDetailsContent = ({ data }: { data: Product }) => {
 
       {images.length > 0 ? (
         <div className="flex items-center gap-2">
-          {images.length > 4 && (
+          {/* 左箭头（有2张以上图片就显示） */}
+          {images.length > 1 && (
             <button
               type="button"
               onClick={() => scroll(-1)}
@@ -29,6 +30,7 @@ const ProductDetailsContent = ({ data }: { data: Product }) => {
             </button>
           )}
 
+          {/* 图片：手机一行1个，电脑一行4个 */}
           <div
             ref={scrollRef}
             className="flex gap-3 overflow-x-auto flex-1"
@@ -37,7 +39,7 @@ const ProductDetailsContent = ({ data }: { data: Product }) => {
             {images.map((img, i) => (
               <div
                 key={i}
-                className="shrink-0 w-1/4 aspect-square bg-[#F0EEED] rounded-[13px] overflow-hidden"
+                className="shrink-0 w-full md:w-1/4 aspect-square bg-[#F0EEED] rounded-[13px] overflow-hidden"
               >
                 <Image
                   src={img}
@@ -50,7 +52,8 @@ const ProductDetailsContent = ({ data }: { data: Product }) => {
             ))}
           </div>
 
-          {images.length > 4 && (
+          {/* 右箭头 */}
+          {images.length > 1 && (
             <button
               type="button"
               onClick={() => scroll(1)}
