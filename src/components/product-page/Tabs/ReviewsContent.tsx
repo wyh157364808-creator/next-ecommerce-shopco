eed890345e106ef7cb9759269bd0d5e58a618cb5
wyh157364808-creator <wyh157364808@gmail.com>
2026-09-16 20:41:37 +0800

    Update ReviewsContent.tsx

--- src/components/product-page/Tabs/ReviewsContent.tsx
+++ src/components/product-page/Tabs/ReviewsContent.tsx
@@ -1,15 +1,7 @@
 import { Button } from "@/components/ui/button";
 import React from "react";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import ReviewCard from "@/components/common/ReviewCard";
 import { reviewsData } from "@/app/page";
-import Link from "next/link";
 
 const ReviewsContent = () => {
   return (
@@ -19,20 +11,11 @@ const ReviewsContent = () => {
           <h3 className="text-xl sm:text-2xl font-bold text-black mr-2">
             All Reviews
           </h3>
-          <span className="text-sm sm:text-base text-black/60">(451)</span>
+          <span className="text-sm sm:text-base text-black/60">
+            ({reviewsData.length})
+          </span>
         </div>
         <div className="flex items-center space-x-2.5">
-          <Select defaultValue="latest">
-            <SelectTrigger className="min-w-[120px] font-medium text-xs sm:text-base px-4 py-3 sm:px-5 sm:py-4 text-black bg-[#F0F0F0] border-none rounded-full h-12">
-              <SelectValue />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="latest">Latest</SelectItem>
-              <SelectItem value="most-relevant">Most Relevant</SelectItem>
-              <SelectItem value="oldest">Oldest</SelectItem>
-            </SelectContent>
-          </Select>
-
           <Button
             type="button"
             className="sm:min-w-[166px] px-4 py-3 sm:px-5 sm:py-4 rounded-full bg-black font-medium text-xs sm:text-base h-12"
@@ -41,21 +24,18 @@ const ReviewsContent = () => {
           </Button>
         </div>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 mb-5 sm:mb-9">
-        {reviewsData.map((review) => (
-          <ReviewCard key={review.id} data={review} isAction isDate />
-        ))}
-      </div>
-      <div className="w-full px-4 sm:px-0 text-center">
-        <Link
-          href="#"
-          className="inline-block w-[230px] px-11 py-4 border rounded-full hover:bg-black hover:text-white text-black transition-all font-medium text-sm sm:text-base border-black/10"
-        >
-          Load More Reviews
-        </Link>
-      </div>
+      {reviewsData.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 mb-5 sm:mb-9">
+          {reviewsData.map((review) => (
+            <ReviewCard key={review.id} data={review} isAction isDate />
+          ))}
+        </div>
+      ) : (
+        <div className="text-center py-12 text-black/50">
+          No reviews yet. Be the first to write one!
+        </div>
+      )}
     </section>
   );
 };
-
 export default ReviewsContent;
