a942af206531c23cfa8bb2b84bbe8b0b3b3403d8
wyh157364808-creator <wyh157364808@gmail.com>
2026-09-18 20:53:28 +0800

    Update CategoriesSection.tsx

--- src/components/shop-page/filters/CategoriesSection.tsx
+++ src/components/shop-page/filters/CategoriesSection.tsx
@@ -1,49 +1,40 @@
 import Link from "next/link";
 import React from "react";
 import { MdKeyboardArrowRight } from "react-icons/md";
-
 type Category = {
   title: string;
   slug: string;
 };
-
 const categoriesData: Category[] = [
   {
-    title: "T-shirts",
-    slug: "/shop?category=t-shirts",
+    title: "Cup",
+    slug: "/shop?category=cup",
   },
   {
-    title: "Shorts",
-    slug: "/shop?category=shorts",
+    title: "Vase",
+    slug: "/shop?category=vase",
   },
   {
-    title: "Shirts",
-    slug: "/shop?category=shirts",
-  },
-  {
-    title: "Hoodie",
-    slug: "/shop?category=hoodie",
-  },
-  {
-    title: "Jeans",
-    slug: "/shop?category=jeans",
+    title: "Tutorial",
+    slug: "/shop?category=tutorial",
   },
 ];
-
 const CategoriesSection = () => {
   return (
-    <div className="flex flex-col space-y-0.5 text-black/60">
-      {categoriesData.map((category, idx) => (
-        <Link
-          key={idx}
-          href={category.slug}
-          className="flex items-center justify-between py-2"
-        >
-          {category.title} <MdKeyboardArrowRight />
-        </Link>
-      ))}
+    <div>
+      <h3 className="font-bold text-black text-base mb-4">Product Type</h3>
+      <div className="flex flex-col space-y-0.5 text-black/60">
+        {categoriesData.map((category, idx) => (
+          <Link
+            key={idx}
+            href={category.slug}
+            className="flex items-center justify-between py-2"
+          >
+            {category.title} <MdKeyboardArrowRight />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
-
 export default CategoriesSection;
