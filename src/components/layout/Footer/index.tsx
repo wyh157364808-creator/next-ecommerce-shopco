8f53dcd317f69b43f1b12b3f8b8bacc61b5d3547
wyh157364808-creator <wyh157364808@gmail.com>
2026-09-20 17:35:05 +0800

    Update index.tsx

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -102,22 +102,8 @@ const Footer = () => {
           <hr className="h-[1px] border-t-black/10 mb-6" />
           <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center mb-2">
             <p className="text-sm text-center sm:text-left text-black/60 mb-4 sm:mb-0 sm:mr-1">
-              © 2026 BEI Pottery. All rights reserved.{" "}
-              <Link
-                href="https://github.com/mohammadoftadeh"
-                className="text-black font-medium"
-              >
-                Mohammad Oftadeh
-              </Link>
-              {", "}
-              Designed by{" "}
-              <Link
-                href="https://www.figma.com/@hamzauix"
-                className="text-black font-medium"
-              >
-                Hamza Naeem
-              </Link>
-            </p>
+  © 2026 BEI Pottery. All rights reserved.
+</p>
             <div className="flex items-center">
               {paymentBadgesData.map((badge, _, arr) => (
                 <span
