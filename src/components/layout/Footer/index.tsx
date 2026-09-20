a8007d55d911ad17fc9d45ca4ea397f3c6149d5f
wyh157364808-creator <wyh157364808@gmail.com>
2026-09-20 17:08:05 +0800

    Update index.tsx

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -74,11 +74,10 @@ const Footer = () => {
                   "text-[28px] lg:text-[32px] mb-6",
                 ])}
               >
-                SHOP.CO
+                BEI Pottery
               </h1>
               <p className="text-black/60 text-sm mb-9">
-                We have clothes that suits your style and which you’re proud to
-                wear. From women to men.
+                Handcrafted Chinese ceramic art. Unique handmade cups and vases for your home.
               </p>
               <div className="flex items-center">
                 {socialsData.map((social) => (
@@ -103,7 +102,7 @@ const Footer = () => {
           <hr className="h-[1px] border-t-black/10 mb-6" />
           <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center mb-2">
             <p className="text-sm text-center sm:text-left text-black/60 mb-4 sm:mb-0 sm:mr-1">
-              Shop.co © Made by{" "}
+              © 2026 BEI Pottery. All rights reserved.{" "}
               <Link
                 href="https://github.com/mohammadoftadeh"
                 className="text-black font-medium"
